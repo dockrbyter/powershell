2cae9dae7948713793488c7a335fe541d5a6254b
thelamescriptkiddiemax <m6ximilian+git46584654@gmail.com>
2020-01-22 19:35:32 +0100

    Darstellung verbessert

--- HardwareInfo.ps1
+++ HardwareInfo.ps1
@@ -1,27 +1,30 @@
 ﻿<#
 HardwareInfos.ps1
-Listet Mainboard, CPU, GPU und RAM Informationen auf
+
+Listet CPU-, Board-, RAM, und GPU-Informationen auf
 #>
+#--- Variablen -----------------------------------------------------------------------------------------------------------------------------------------
 
-#-----------------------------------------------------------------------------------------------
-$cpuinfos = Get-WmiObject -Class Win32_Processor
-$boarinfos = Get-WmiObject -Class Win32_BaseBoard -ComputerName $computer
-$raminfo = Get-WmiObject -Class Win32_PhysicalMemory -ComputerName $computer
-$gpuinfos = Get-Wmiobject Win32_VideoController | Select-Object -Property -Property Name
+$cpuinfos = Get-WmiObject -Class Win32_Processor | Select-Object -Property Name
+$boardinfos = Get-WmiObject -Class Win32_BaseBoard | Select-Object -Property Manufacturer, Product
+$raminfos = Get-WmiObject -Class Win32_PhysicalMemory | Select-Object -Property Manufacturer, Capacity
+$gpuinfos = Get-Wmiobject Win32_VideoController | Select-Object -Property Name, AdapterRAM, CurrentHorizontalResolution, CurrentVerticalResolution
 
-$computer = $env:computername
 
-#-----------------------------------------------------------------------------------------------
+#--- Verarbeitung ---------------------------------------------------------------------------------------------------------------------------------------
 
 Clear-Host
 
-Write-Host "   GPU: $gpuinfos"
-
-
-
-
-
-
-
+Write-Host " "
+Write-Host " "
+Write-Host "   System   : " $env:computername
+Write-Host " "
+Write-Host "   CPU      : " $cpuinfos
+Write-Host "   Board    : " $boardinfos
+Write-Host "   RAM      : " $raminfos
+Write-Host "   GPU      : " $gpuinfos
+Write-Host " "
+Write-Host " "
 
 pause
+

aa540e61e1a40c249f752d7a13df4715572474a9
Max <m6ximilian@gmail.com>
2020-01-26 23:54:45 +0100

    Userstuff gefixt

--- soest/notebookhelperSO.ps1
+++ soest/notebookhelperSO.ps1
@@ -8,14 +8,16 @@ Hilft Notebooks nach der Windowsinstallation nach Hause zu finden
 
 $neuerName = "NB02"                 # Gewuenschter Name
 
-$loeschuser = "SIT"                 # Zu loeschender user BSP "DummerUser4"
+$loeschuser = "User12"                 # Zu loeschender user BSP "DummerUser4"
+$userPWbearbeitung = "Admin"        # User dessen Passwort geaendert werden soll BSP "User13"
 
 $wlanProfil1 = "wlan.xml"             # Zu importierendes Profil 1
 $wlanProfil2 = "wlan.xml"             # Zu importierendes Profil 2
+    $wlanDelay = "10"               # Zu wartende Zeit nach WLAN-Beitritt um auf Adaptnerneustart zu warten in Sekunden BSP "10"
 
 $proxyserver = "10.10.10.10:8080"   # Proxy + Port BSP: "172.16.0.1:8080"
 
-$domaintojoin = "schule.mnsplus"        # Domainname BSP: "pettingzoo.party"
+$domaintojoin = "domain.local"        # Domainname BSP: "pettingzoo.party"
     #$oupfad = "OU=Noteboocks,OU=Raum 1408,DC=ad,DC=pettingzoo,DC=party"             # Der OU-Pfad (distinguishedName) BSP: "OU=Noteboocks,OU=Raum 1408,DC=ad,DC=pettingzoo,DC=party"
 
 $delay = "5"                       # Wartezeit nach Abschluss fuer Neustart (Script hat eigenes Delay von 5 Sekunden)
@@ -25,19 +27,30 @@ $delay = "5"                       # Wartezeit nach Abschluss fuer Neustart (Scr
 # Phase 0 - Begruessung und Credentials
 
 Clear-Host
+Write-Host " "
+Write-Host " "
 Write-Host "   -- Notebook Helper -- "
+Write-Host " "
+Write-Host " "
 Write-Host " "
 Start-Sleep -Milliseconds 500
 
 Clear-Host
+Write-Host " "
+Write-Host "    HOST: $env:computername "
+Write-Host " "
+Write-Host " "
 Write-Host "   Hey hey hey!"
 Write-Host "   Hast du dich verlaufen, kleines Notebook?"
 Write-Host "   Ich helfe dir nach Hause..."
+Write-Host " "
 Write-Host " "
 Start-Sleep -Seconds 1
 Clear-Host
 
 Clear-Host
+Write-Host " " 
+Write-Host " "
 Write-Host "   Domain Credentials benoetigt!"
 Write-Host " "
 Write-Host " " 
@@ -47,38 +60,47 @@ $domaincredential = Get-Credential
 pause
 
 Clear-Host
+Write-Host " "
+Write-Host " "
 Write-Host "        Neues Passwort"
-Write-Host "        fuer lokaler Administrator "
+Write-Host "        $userPWbearbeitung "
 Write-Host "        benoetigt!"
 Write-Host " "
 
-$adminpw = read-host "Passwort lokaler Administrator" -asSecureString                  # WLAN Key 1
+$neuespw = read-host "Passwort $userPWbearbeitung" -asSecureString                  # WLAN Key 1
 
 
 #--- Phase I - Name, User, WLAN1 und Updates -------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 Clear-Host
+Write-Host " "
+Write-Host " "
 Write-Host "        Phase 1"
 Write-Host " "
 Start-Sleep -Seconds 1
 Clear-Host
 
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 Remove-LocalUser -Name $loeschuser                              # Ueberfluessiger User loeschen
 
-pause
-
-Administrator | Set-LocalUser -Password $adminpw
-
-pause
+$userX = Get-LocalUser -Name $userPWbearbeitung                 # Get-LocalUser binden
+$userX | Set-LocalUser -Password $neuespw                       # Passwort fuer User aendern
 
 Clear-Host
+Write-Host " "
+Write-Host " "
 Write-Host " Ueberfluessinger User $loeschuser entfernt!"
-Write-Host " Passwort fuer lokalen Administrator gesetzt!"
+Write-Host " Passwort fuer $userPWbearbeitung gesetzt!"
 Write-Host " "
 Start-Sleep -Milliseconds 500
 Clear-Host 
 
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 Clear-Host
+Write-Host " "
+Write-Host " "
 Write-Host " Benenne Computer $env:computername in $neuerName um..."
 Write-Host " "
 Start-Sleep -Milliseconds 500
@@ -86,26 +108,28 @@ Clear-Host
 
 Rename-Computer -NewName $neuerName
 
-pause
-
     Clear-Host
+    Write-Host " "
+    Write-Host " "
     Write-Host " Erledigt... Trete WLAN1  $ssid1  bei..."
     Write-Host " "
     Start-Sleep -Milliseconds 500
     Clear-Host
 
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 netsh wlan add profile filename="$PSScriptRoot\$wlanProfil1" user=all                # WLAN join 1
 Restart-NetAdapter
-
-
     
-    Clear-Host    
-    Write-Host " WLAN1  $ssid1  verbunden! Mache 10 Sek Pause... Lade dann Updates..."
+    Clear-Host
     Write-Host " "
-    Start-Sleep --Seconds 10
+    Write-Host " "
+    Write-Host " WLAN1  $wlanProfil1  verbunden! Mache $wlanDelay Sek Pause... Lade dann Updates..."
+    Write-Host " "
+    Start-Sleep --Seconds $wlanDelay
     Clear-Host
 
-    
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------    
 
 Install-Module -Name PSWindowsUpdate –Force             # PowerShell Update-Modul installieren
 Import-Module PSWindowsUpdate                           # PowerShell Update-Modul importieren
@@ -113,16 +137,23 @@ Import-Module PSWindowsUpdate                           # PowerShell Update-Modu
     Download-WindowsUpdate -ForceDownload               # Updates downloaden
     Install-WindowsUpdate -ForceInstall                 # Updates installieren
 
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
     Clear-Host
-    Write-Host " Updates installiert! Entferne WLAN1  $ssid1  ..."
+    Write-Host " "
+    Write-Host " "
+    Write-Host " Updates installiert! Entferne WLAN1  $wlanProfil1  ..."
     Write-Host " " 
     Start-Sleep -Milliseconds 500
     Clear-Host
 
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 Netsh wlan delete profile $wlanProfil1                        # WLAN1 Profil loeschen
 
     Clear-Host
+    Write-Host " "
+    Write-Host " "
     Write-Host " WLAN1  $wlanProfil1  entfernt!"
     Write-Host " "
     Start-Sleep -Milliseconds 500
@@ -131,39 +162,57 @@ Netsh wlan delete profile $wlanProfil1                        # WLAN1 Profil loe
 #--- Phase II - WLAN2, Proxy und Domainjoin -------------------------------------------------------------------------------------------------------------------
 
 Clear-Host
+Write-Host " "
+Write-Host " "
 Write-Host "        Phase 2"
 Write-Host " "
 Start-Sleep -Seconds 1
 Clear-Host
 
 Clear-Host
+Write-Host " "
+Write-Host " "
 Write-Host " Setze Proxy und trete WLAN2  $wlanProfil2  bei..."
 Write-Host " "
 Start-Sleep -Milliseconds 500
 Clear-Host
 
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 netsh winhttp set proxy $proxyserver                    # Proxy setzen
 
-netsh wlan add profile filename="$PSScriptRoot\$wlanProfil1" user=all                # WLAN join 2
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+netsh wlan add profile filename="$PSScriptRoot\$wlanProfil2" user=all                # WLAN join 2
 Restart-NetAdapter
 
     Clear-Host
+    Write-Host " "
+    Write-Host " "
     Write-Host " Proxy gesetzt!"
-    Write-Host " WLAN2  $wlanProfil1  verbunden! Mache 10 Sek Pause, "
+    Write-Host " WLAN2  $wlanProfil2  verbunden! Mache $wlanDelay Sek Pause, "
     Write-Host " trete dann Domain $domaintojoin bei..."
     Write-Host " "
-    Start-Sleep -Seconds 10
+    Start-Sleep -Seconds $wlanDelay
     Clear-Host
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 Add-Computer -DomainName $domaintojoin -Credential $domaincredential  # -OUPath $oupfad      # Domainjoin
 
     Clear-Host
+    Write-Host " "
+    Write-Host " "
     Write-Host " Domain  $domaintojoin  beigetreten!"
     Write-Host " "
     Start-Sleep -Milliseconds 500
     Clear-Host
 
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 Clear-Host
+Write-Host " "
+Write-Host " "
 Write-Host "    Alles Erledigt!"
 Write-Host "    Das Notebook $env:computername ( $neuerName )"
 Write-Host "    ist jetzt zuhause..."
@@ -172,6 +221,8 @@ Start-Sleep -Seconds 5
 Clear-Host
 
 Clear-Host
+Write-Host " "
+Write-Host " "
 Write-Host "   -- Notebook Helper --"
 Write-Host " "
 Start-Sleep -Milliseconds 300
@@ -179,32 +230,34 @@ Start-Sleep -Milliseconds 300
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 Clear-Host
+Write-Host " "
+Write-Host " "
 Write-Host "   Neustart in $delay Sekunden"
-Write-Host "       Plus Fünf"
+Write-Host "       +5"
 Write-Host " "
 Start-Sleep -Seconds 1
 
 Write-Host "   Neustart in $delay Sekunden"
-Write-Host "      Plus Vier"
+Write-Host "       +4"
 Write-Host " "
 Start-Sleep -Seconds 1
 
 Write-Host "   Neustart in $delay Sekunden"
-Write-Host "       Plus Drei"
+Write-Host "       +3"
 Write-Host " "
 Start-Sleep -Seconds 1
 
 Write-Host "   Neustart in $delay Sekunden"
-Write-Host "       Plus Zwei"
+Write-Host "       +2"
 Write-Host " "
 Start-Sleep -Seconds 1
 
 Write-Host " Neustart in $delay Sekunden "
-Write-Host " Plus Eins"
-
-
+Write-Host "       +1"
 Start-Sleep -Seconds 1
 
+Write-Host " Neustart in $delay Sekunden "
 
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 Restart-Computer -Wait Delay $delay -Force           # Neustart

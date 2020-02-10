1ad3cf2f2a8dabd5ffb0065ae70d4fdf2bfc2189
Max <m6ximilian@gmail.com>
2020-02-10 19:26:39 +0100

    init

---
+++ soest/netsharestarter.ps1
@@ -0,0 +1,33 @@
+<#
+netsharestarter.ps1
+
+    - Geplante Task erstellen
+        - Benutzer oder Gruppe auswaehlen BSP LocalComputer\Users
+        - Neuer Trigger: Beim Anmelden
+        - Aktion: Programm starten
+            * Powershell.exe
+            * Argumente: -windowsstyle hidden -command .\netsharestarter.ps1
+            * Starten in: Speicherort des Scripts
+        * Nur starten wenn folgende Netzwerkverbindung verfuegbar ist: Beliebige NIC 
+
+#>
+#--- Vorbereitung -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+$i=3
+while($True){
+    $error.clear()
+    $MappedDrives = Get-SmbMapping |where -property Status -Value Unavailable -EQ | select LocalPath,RemotePath
+    foreach( $MappedDrive in $MappedDrives)
+    {
+        try {
+            New-SmbMapping -LocalPath $MappedDrive.LocalPath -RemotePath $MappedDrive.RemotePath -Persistent $True
+        } catch {
+            Write-Host "There was an error mapping $MappedDrive.RemotePath to $MappedDrive.LocalPath"
+        }
+    }
+    $i = $i - 1
+    if($error.Count -eq 0 -Or $i -eq 0) {break}
+
+    Start-Sleep -Seconds 30
+
+}

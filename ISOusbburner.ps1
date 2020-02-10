aa26192d3b9e87237b150aa310daf54b626a898f
Max <m6ximilian@gmail.com>
2020-02-10 17:51:21 +0100

    date format

--- ISOusbburner.ps1
+++ ISOusbburner.ps1
@@ -13,7 +13,8 @@ ISOusbburner.ps1
 #--- Vorbereitung -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 $stringhost = [System.String]::Concat("`n", "[ ", $env:UserName, " @ ", $env:computername, " @ ", ((Get-WmiObject Win32_ComputerSystem).Domain), " @ Windows 10: ", 
-((Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\" -Name ReleaseID).ReleaseId), " ]   ", (Get-Date -Format “dd/MM/yyyy HH:mm:ss”), "`n", "[ ", $MyInvocation.MyCommand.Name, " ]", "`n","`n")
+((Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\" -Name ReleaseID).ReleaseId), " ]   ", (Get-Date -Format "dd/MM/yyyy HH:mm:ss"), "`n", "[ ", $MyInvocation.MyCommand.Name, " ]", "`n","`n")
+
 $stringhallo = [System.String]::Concat("   Hallo ", $env:UserName, "!", "`n")
 
 #--- Verarbeitung -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------

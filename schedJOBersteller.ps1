27dd57be2ca1489f9fe4fb60c529d538304b5943
Max <m6ximilian@gmail.com>
2020-02-12 19:19:54 +0100

    format

--- schedJOBersteller.ps1
+++ schedJOBersteller.ps1
@@ -1,7 +1,7 @@
 <#
 taskersteller.ps1
 
-    Erstellt geplante Tasks fuer Scripts
+    Erstellt geplante Jobs fuer Scripts
 
 #>
 #--- Variablen ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -20,26 +20,33 @@ $scriptjobname = $taskscript.Replace(".ps1", "")
 $stringhost = [System.String]::Concat("`n", "[ ", $env:UserName, " @ ", $env:computername, " @ ", ((Get-WmiObject Win32_ComputerSystem).Domain), " ", (Get-CimInstance Win32_OperatingSystem | Select-Object Caption), ": ", 
 ((Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\" -Name ReleaseID).ReleaseId), " ]   ", (Get-Date -Format "dd/MM/yyyy HH:mm:ss"), "`n", "[ ", $MyInvocation.MyCommand.Name, " ]", "`n","`n") 
 $stringhost = $stringhost.replace("{Caption=Microsoft"," ")
+$string1 = [System.String]::Concat("   Erstelle Job ", $scriptjobname, "`n", "`n")
+$string2 = [System.String]::Concat("   Joberstellung: ", $jobtest, " !", "`n", "`n")
+
+$jobtesterfolg = "Erfolgreich"
+$jobtestfail = "NICHT erfolgreich"
+$jobtest = $jobtestfail
 
 #--- Verarbeitung -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 Clear-Host
 Write-Host $stringhost -ForegroundColor Magenta
-Write-Host " "
-Start-Sleep -Seconds 1
+Write-Host "      FUEHRE MICH ALS ADMIN AUS!" -ForegroundColor DarkRed
+Start-Sleep -Seconds 2
 
+Clear-Host
+Write-Host $stringhost -ForegroundColor Magenta
+Write-Host $string1
+Start-Sleep -Seconds 2
 
+Register-ScheduledJob -Name $scriptjobname -FilePath $scripttarget -ScheduledJobOption $schedoptions -Trigger $schedtrigger     # Erstelle geplanten Job
 
-Register-ScheduledJob -Name $scriptjobname -FilePath $scripttarget -ScheduledJobOption $schedoptions -Trigger $schedtrigger
-
-
-
-
-
-if ( Get-ScheduledTask $scriptjobname -ErrorAction Ignore ) { $smbtesttask = $smbtesterfolg }       # Checke geplante Task
+if ( Get-ScheduledTask $scriptjobname -ErrorAction Ignore ) { $jobtest = $jobtesterfolg }       # Checke geplanten Job
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-Write-Host $smbtesttask
-
+Clear-Host
+Write-Host $stringhost -ForegroundColor Magenta
+Write-Host $string2
+Start-Sleep -Seconds 4
 

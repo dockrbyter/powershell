149f71ba2fdfbe5b3a719fc63c66d3465483efe1
Max <m6ximilian@gmail.com>
2020-02-15 11:40:07 +0100

    vhd function

--- Hyper-V/thrallconstructor.ps1
+++ Hyper-V/thrallconstructor.ps1
@@ -9,7 +9,7 @@ https://github.com/thelamescriptkiddiemax/powershell/Hyper-V
 #>
 #--- Variablen ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-$switchName = "IntPowerVswitch"    # V-Switch Name
+$vhdpath = "$env:PUBLIC\Documents\Hyper-V\Virtual Hard Disks\"      # Speicherort VHD - Standard: $env:PUBLIC\Documents\Hyper-V\Virtual Hard Disks\
 
 
 
@@ -44,12 +44,12 @@ function vmmaker {
     Read-Host "  VM-Name?   BSP: Thrall03" $vmname
     Read-Host "  Wie viele Cores?   BSP: 4" $vmcores
     Read-Host "  VM RAM in GB?   BSP: 16" $vmRAMraw
-    Read-Host "  VHD-Pfad?   BSP: C:\vms\vhd" $vmVHDpfad
+    Read-Host "  VHD-Pfad?   BSP: C:\vms\vhd" $vhdpath
     Read-Host "  VHD-Name?   BSP: SVR19HD   (NUR VHDX - VHD nicht)" $vmVHDname
     Read-Host "  vSwitch-Name?   BSP: extVswitch" $switchName
 
     $vmRAM = [System.String]::Concat($vmRAMraw, "GB")
-    $vmVHD = [System.String]::Concat($vmVHDpfad, "\", $vmVHDname, ".vhdx")
+    $vmVHD = [System.String]::Concat($vhdpath, "\", $vhdname, ".vhdx")
     $stringVMerstellt = [System.String]::Concat("   VM ", $vmname, " erstellt!", "`n", "   Snapshots bitte selber konfigurieren!")
 
     Clear-Host
@@ -72,15 +72,40 @@ function vmmaker {
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function vhdmaker {
 
+    Write-Host $stringhost -ForegroundColor Magenta
+    Write-Host "Zielverzeichnis in Script-Variablen konfigurieren!" -ForegroundColor DarkRed
+    Read-Host "   VHD-Name?" $vhdname
+    Read-Host "   VHD-Groesse?" $vhdsize
     
+    Write-Host "   (F)ixe-, oder (D)ynamische-Groesse? Default - Fix"
+    $Readhost = Read-Host " ( F / D ) "
+    Switch ($ReadHost) 
+    {
+        F {$vhdtyp = "Fix"; Clear-Host; Write-Host $stringhost -ForegroundColor Magenta;  Write-Host $stringintro; Start-Sleep -Seconds 2;
+        New-VHD -Path $vhdpath -Fixed -SizeBytes (Invoke-Expression $vhdsize)} 
+
+        D {$vhdtyp = "Dynamisch"; Clear-Host; Write-Host $stringhost -ForegroundColor Magenta;  Write-Host $stringintro; Start-Sleep -Seconds 2;
+        New-VHD -Path $vhdpath -Dynamic -SizeBytes (Invoke-Expression $vhdsize)}
+
+        Default {$vhdtyp = "Fix"; Clear-Host; Write-Host $stringhost -ForegroundColor Magenta;  Write-Host $stringintro; Start-Sleep -Seconds 2;
+        New-VHD -Path $vhdpath -Fixed -SizeBytes (Invoke-Expression $vhdsize)} 
+ }
     
+    Write-Host $stringhost -ForegroundColor Magenta
+    Write-Host "   VHD erstellt!"
+    Start-Sleep -Seconds 1.5
+
 }
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
 #--- Verarbeitung -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 Clear-Host
 Write-Host $stringhost -ForegroundColor Magenta
 Write-Host "   ThrallConstructor"
-Start-Sleep -Seconds 1
+Start-Sleep -Seconds 1.5
 
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -90,11 +115,11 @@ do{
      $input = Read-Host "Bitte waehlen"
      switch ($input)
      {
-        1 { }      # Aktion Taste 1
-        2 { }     # Aktion Taste 2
-		3 { }          # Aktion Taste 3
-		4 {vmmaker}         # Aktion Taste 4
-        5 { }  # Aktion Taste 5
+        1 { }      # Aktion Taste 1 - 
+        2 { }     # Aktion Taste 2 - 
+		3 { vhdmaker }          # Aktion Taste 3 - VHDs erstellen
+		4 { vmmaker }         # Aktion Taste 4 - VMs montieren
+        5 { }  # Aktion Taste 5 - 
      }       
 }
 until ($input -eq 'q') 

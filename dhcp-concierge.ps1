f223b233a82bd734fb868f88417a5f9ea0f6502e
Max <m6ximilian@gmail.com>
2020-05-25 09:58:23 +0200

    testing

--- dhcp-concierge.ps1
+++ dhcp-concierge.ps1
@@ -2,101 +2,57 @@
 dhcp-concierge.ps1
 .DESCRIPTION
 
-    Script-Vorlage
+    Erstellt DHCP-Bereiche anahnd einer CSV-Datei
+    und ordnet Clients anhand einer weiteren CSV-Datei
+    den ensprechenden Bereichen zu. 
+    Drucker- und Rest-Bereich bitte NICHT inder CSV angeben!
+    Einstellungen hierfuer bitte in Variablen vornehmen.
     
 https://github.com/thelamescriptkiddiemax/powershell
 #>
 #--- Variablen ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-$poolcsv = "C:\dhcp\pool.csv"
-$clientcsv = "C:\dhcp\clients.csv"
+$poolcsv = ("$PSScriptRoot\pool.csv")       # Raum-CSV-Datei                                            EX  C:\dhcp\pool.csv
+$clientcsv = ("$PSScriptRoot\clients.csv")  # Client-CSV-Datei                                          EX  C:\dhcp\clients.csv
 
-$dchpServer = ""          # Hostname des DHCP-Servers
+$dchpServer = ""                            # Hostname des DHCP-Servers                                 EX  DCHPSRV01
 
-$domainname = ""
-$domainsuffix = ""
+$domainname = ""                            # Domain-Name                                               EX  standort01
+$domainsuffix = ""                          # Domain-Suffix                                             EX  local
 
-$ippref = "10.10"
-$iprangestart = "100"
-$iprangeend = "200"
-$sunbnetmask = "255.255.0.0"
+$ippref = "10.10"                           # Erstes Oktet der IP-Range                                 EX  192.168
+$iprangestart = "100"                       # Erste IP-Adresse eines Pools                              EX  100
+$iprangeend = "200"                         # Letzte IP-Adresse eines Pools                             EX  200
+$sunbnetmask = "255.255.0.0"                # Subnetmaks                                                EX  255.255.0.0
 
-$raeume = @(
-    "NBPHY*",
-    "NBMIM*",
-    "NBHOLZ",
-    "*E205*",
-    "*E115*",
-    "*F206*",
-    "*F205*",
-    "*F204*",
-    "*F202*",
-    "*F201*",
-    "*F121*",
-    "*F118*",
-    "*F117*",
-    "*F115*",
-    "*F112*",
-    "*F108*",
-    "*F107*",
-    "*F104*",
-    "*F101*",
-    "*F020*",
-    "*F018*",
-    "*F011*",
-    "*F004*",
-    "*F003*",
-    "*E216*",
-    "*E212*",
-    "*E211*",
-    "*E210*",
-    "*E206*",
-    "*E112*",
-    "*E107*",
-    "*E105*",
-    "*E104*",
-    "*E103*",
-    "*E001*",
-    "*D217*",
-    "*D216*",
-    "*D215*",
-    "*D214*",
-    "*D212*",
-    "*D205*",
-    "*D203*",
-    "*D202*",
-    "*D112*",
-    "*D106*",
-    "*D105*",
-    "*D103*",
-    "*D102*",
-    "*D009*",
-    "*D004*",
-    "*D003*",
-    "*D002*",
-    "*D001*",
-    "*C306*",
-    "*C301*",
-    "*C111*",
-    "*C102*",
-    "*C101*",
-    "*BIBLIO*"
+$raumcount = "99"                           # Startbereich der automatischen Bereichserstellund (-1)    EX  99 -wenn erster erstellter Bereich 100 sein soll
+$printerpool = "Drucker"                    # Bereichsbezeichnung fuer Durcker                          EX  Drucker
+$printerrange = "30"                        # Breich fuer Drucker                                       EX  30
+$restpool = "Sonstige"                      # Bereich fuer Geraete die nicht zugeordnet werden koennen  EX  Sonstige
+$restrange = "200"                          # Breich fuer Sonstige Geraete                              EX  200
 
-)
-
-
-$scriptspeed = "2"
+$scriptspeed = "2"                          # Darstellungsdauer der Textausgaben in Sekunden            EX  2
 
 #--- Vorbereitung -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 $stringhost = [System.String]::Concat("[ ", $env:UserName, " @ ", $env:computername, " @ ", ((Get-WmiObject Win32_ComputerSystem).Domain), " ", (Get-CimInstance Win32_OperatingSystem | Select-Object Caption), ": ", 
 ((Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\" -Name ReleaseID).ReleaseId), " ]   ", (Get-Date -Format "dd/MM/yyyy HH:mm:ss"), "`n", "[ ", $MyInvocation.MyCommand.Name, " ]", "`n","`n") 
 $stringhost = $stringhost.replace("{Caption=Microsoft"," ").replace("}", " ")
-
-$dhcpclients = Get-DHCPServerV4Scope -ComputerName $dchpServerIp | ForEach-Object { Get-DHCPServerv4Lease -ScopeID $_.ScopeID } | Select-Object HostName, ClientID
-
+$stringtitle = "`n* * *              * * *`n     DHCP-Concierge     `n* * *              * * *`n"
 
 $dhcpsrv = ("$dchpServer.$domainname.$domainsuffix")
+
+$printerrangestart = ("$ippref.$printerrange.$iprangestart")
+$printerrangeend = ("$ippref.$printerrange.$iprangeend")
+
+$restrangestart = ("$ippref.$restrange.$iprangestart")
+$restrangeend = ("$ippref.$restrange.$iprangeend")
+
+$headraum = "Raeume"
+$headScopeId = "ScopeId"
+$headclientname = "Name"
+$headclientmac = "ClientId"
+$headfull =   @("$headScopeId,$headclientname,$headclientmac")
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function waittimer {
@@ -104,50 +60,129 @@ function waittimer {
 }
 
 #--- Verarbeitung -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#--- Datei-Checks -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 Clear-Host
 Write-Host $stringhost -ForegroundColor Magenta
-Write-Host "   dhcp-concierge"
+Write-Host $stringtitle
 waittimer
 
-
-# DHCP Pools erstellen
 Clear-Host
 Write-Host $stringhost -ForegroundColor Magenta
-Write-Host "   dhcp-concierge"
+Write-Host $stringtitle
+Write-Host "`n   Suche nach CSV-Dateien...`n"
+waittimer
+
+# Raum-CSV checken und bei Bedarf erstellen
+if (!(Test-Path $poolcsv)) {
+    
+    Clear-Host
+    Write-Host $stringhost -ForegroundColor Magenta
+    Write-Host $stringtitle
+    Write-Host "`n   Raum-CSV-Datei nicht gefunden! `n   Erstelle Datei...`n"
+    waittimer
+
+    Add-Content -Path $poolcsv -Value $headraum
+
+    Clear-Host
+    Write-Host $stringhost -ForegroundColor Magenta
+    Write-Host $stringtitle
+    Write-Host "`n   Raum-CSV-Datei erstellt! `n   Bitte befuellen und Script neu starten.`n"
+    waittimer
+
+    stop-process -Id $PID       # Shell schliessen
+
+}
+
+# Client-CSV checken und bei Bedarf erstellen
+if (!(Test-Path $clientcsv)) {
+    
+    $stringheaderinfo = [System.String]::Concat("`n   ", $headScopeId, " = Raumname`n   ", $headclientname, " = Client-Host-Name`n   ", $headclientmac, " = CLient-MAC-Adresse`n`n")
+
+    Clear-Host
+    Write-Host $stringhost -ForegroundColor Magenta
+    Write-Host $stringtitle
+    Write-Host "`n   Client-CSV-Datei nicht gefunden! `n   Erstelle Datei...`n"
+    waittimer
+
+    Add-Content -Path $clientcsv -Value $headfull
+
+    Clear-Host
+    Write-Host $stringhost -ForegroundColor Magenta
+    Write-Host $stringtitle
+    Write-Host "`n   Client-CSV-Datei erstellt! `n   Bitte befuellen und Script neu starten.`n"
+    Write-Host $stringheaderinfo
+    waittimer
+
+    stop-process -Id $PID       # Shell schliessen
+
+}
+
+
+#--- DHCP Pools erstellen -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Clear-Host
+Write-Host $stringhost -ForegroundColor Magenta
+Write-Host $stringtitle
 Write-Host "`n   Beginne mit Raumerstellung...`n"
 waittimer
 
-$raumcount = "0"
+Clear-Host
+Write-Host $stringhost -ForegroundColor Magenta
+Write-Host $stringtitle
+Write-Host "`n   Erstelle Drucker-Bereich...`n"
+waittimer
 
-Import-Csv -Path $poolcsv -Header Raeume |  Foreach-Object{
+Add-DhcpServerv4Scope -ComputerName $dhcpsrv -Name $printerpool -StartRange $printerrangestart -EndRange $printerrangeend -SubnetMask $sunbnetmask -Description $printerpool
+
+Clear-Host
+Write-Host $stringhost -ForegroundColor Magenta
+Write-Host $stringtitle
+Write-Host "`n   ...erledigt! Erstelle Sonstigen-Bereich...`n"
+waittimer
+
+Add-DhcpServerv4Scope -ComputerName $dhcpsrv -Name $restpool -StartRange $restrangestart -EndRange $restrangeend -SubnetMask $sunbnetmask -Description $printerpool
+
+Clear-Host
+Write-Host $stringhost -ForegroundColor Magenta
+Write-Host $stringtitle
+Write-Host "`n   ...erledigt! Beginne mit CSV-Verarbeitung...`n"
+waittimer
+
+Import-Csv -Path $poolcsv -Header $headraum |  Foreach-Object{
     $raumcount++
     $stringcurrent = [System.String]::Concat("`n   DHCP-Pool ", $_.Raeume, "`n   Range von: ", $raumSTARTrange, " bis ", $raumENDrange, "`n")
     
     $raumSTARTrange = [System.String]::Concat("$ippref.$raumcount.$iprangestart")
     $raumENDrange = [System.String]::Concat("$ippref.$raumcount.$iprangeend")
+    
     Add-DhcpServerv4Scope -ComputerName $dhcpsrv -Name $_.Raeume -StartRange $raumSTARTrange -EndRange $raumENDrange -SubnetMask $sunbnetmask -Description $_.Raeume
 
     Clear-Host
     Write-Host $stringhost -ForegroundColor Magenta
-    Write-Host "   dhcp-concierge"
+    Write-Host $stringtitle
     Write-Host $stringcurrent
     waittimer
     }
 
 
-# Clients den Pools zuweisen
+#--- Clients den Pools zuweisen -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 Clear-Host
 Write-Host $stringhost -ForegroundColor Magenta
-Write-Host "   dhcp-concierge"
-Write-Host "`n   Ich zeige nun unseren Gaesten ihre Zimmer...`n"
+Write-Host $stringtitle
+Write-Host "`n   Ich zeige nun unseren Gaesten ihre Zimmer...`n   (Ordne Clients DHCP-Bereichen)`n"
 waittimer
 
-Import-Csv -Path $dhcpclients -Header Raeume  | Add-DhcpServerv4Scope -ComputerName $dhcpsrv -State Active
+Import-Csv -Path $clientcsv -Header $headraum  | Add-DhcpServerv4Scope -ComputerName $dhcpsrv -State Active
 
-
-
+Clear-Host
+Write-Host $stringhost -ForegroundColor Magenta
+Write-Host $stringtitle
+Write-Host "`n   Unsere Gaeste befinden sich nun auf ihren Zimmern.`n   (Bereichszuordnung abgeschlossen)`n"
+waittimer
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 stop-process -Id $PID       # Shell schliessen
+

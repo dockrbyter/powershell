8bff2ca8ee4af948d7df7128f337d3764aa38f27
Max <m6ximilian@gmail.com>
2020-05-25 12:19:40 +0200

    upgrade...

--- 11aa_VORLAGE.ps1
+++ 11aa_VORLAGE.ps1
@@ -11,18 +11,24 @@ https://github.com/thelamescriptkiddiemax/powershell
 
 
 
+$scriptspeed = "2"                          # Darstellungsdauer der Textausgaben in Sekunden            EX  2
+
 #--- Vorbereitung -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 $stringhost = [System.String]::Concat("[ ", $env:UserName, " @ ", $env:computername, " @ ", ((Get-WmiObject Win32_ComputerSystem).Domain), " ", (Get-CimInstance Win32_OperatingSystem | Select-Object Caption), ": ", 
 ((Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\" -Name ReleaseID).ReleaseId), " ]   ", (Get-Date -Format "dd/MM/yyyy HH:mm:ss"), "`n", "[ ", $MyInvocation.MyCommand.Name, " ]", "`n","`n") 
 $stringhost = $stringhost.replace("{Caption=Microsoft"," ").replace("}", " ")
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function waittimer {
+    Start-Sleep -Seconds $scriptspeed
+}
 
 #--- Verarbeitung -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 Clear-Host
 Write-Host $stringhost -ForegroundColor Magenta
 Write-Host " "
-Start-Sleep -Seconds 1
+waittimer
 
 pause
 

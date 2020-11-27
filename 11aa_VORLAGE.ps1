f63ed4870c9e4d109ba43e795e52edebd8a0b0af
thelamescriptkiddiemax <60104113+thelamescriptkiddiemax@users.noreply.github.com>
2020-11-27 21:24:09 +0100

    Update Formatierungen

--- 11aa_VORLAGE.ps1
+++ 11aa_VORLAGE.ps1
@@ -11,31 +11,55 @@ https://github.com/thelamescriptkiddiemax/powershell
 
 
 
-$scriptspeed = "2"                          # Darstellungsdauer der Textausgaben in Sekunden            EX  2
+$scriptspeed = "2"                                                                  # Darstellungsdauer der Textausgaben in Sekunden                                            EX  2
+$fmode = "x"                                                                        # Floating Mode (fuer Debugging)                                                            EX  x
 
 #--- Vorbereitung -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-$stringhost = [System.String]::Concat("[ ", $env:UserName, " @ ", $env:computername, " @ ", ((Get-WmiObject Win32_ComputerSystem).Domain), " ", (Get-CimInstance Win32_OperatingSystem | Select-Object Caption), ": ", 
-((Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\" -Name ReleaseID).ReleaseId), " ]   ", (Get-Date -Format "dd/MM/yyyy HH:mm:ss"), "`n", "[ ", $MyInvocation.MyCommand.Name, " ]", "`n","`n") 
-$stringhost = $stringhost.replace("{Caption=Microsoft"," ").replace("}", " ")
-#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-function waittimer {
-    Start-Sleep -Seconds $scriptspeed
+
+
+$scriptname = $MyInvocation.MyCommand.Name
+
+#--- Funktionen ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Scripthead
+function scripthead
+{
+
+    # create stringhost
+    $stringhost = [System.String]::Concat("[ ", $env:UserName, " @ ", $env:computername, " @ ", ((Get-WmiObject Win32_ComputerSystem).Domain), " ", (Get-CimInstance Win32_OperatingSystem | Select-Object Caption), ": ", 
+    ((Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\" -Name ReleaseID).ReleaseId), " ]   ", (Get-Date -Format "dd/MM/yyyy HH:mm"), "`n", "[ ", $scriptname, " ]", "`n","`n") 
+    $stringhost = $stringhost.replace("{Caption=Microsoft"," ").replace("}", " ")
+
+    # fmode
+    if (!$fmode)
+    {
+        Clear-Host                                                                                                                                  # Clear Screen
+    }
+
+    Write-Host $stringhost -ForegroundColor Magenta
+
+    Write-Host "   Titel" -ForegroundColor Blue
+
+    Write-Host "`n"
+
+}
+
+# Dauer Einblendungen
+function scriptspeed ($scriptspeed)
+{
+    Start-Sleep -Seconds $scriptspeed                                                                                                               # display timeout
 }
 
 #--- Verarbeitung -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-Clear-Host
-Write-Host $stringhost -ForegroundColor Magenta
-Write-Host " "
-waittimer
+scripthead
+waittimer $scriptspeed
 
-pause
+
+
+
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-
-
-
-stop-process -Id $PID       # Shell schliessen
-
+#stop-process -Id $PID       # Shell schliessen
